d5c3ca7e7fef9a5c16686c57835b13a772144405
Baptiste Firket <Baptiste.Firket@stepstone.be>
2021-04-26 02:49:36 +0200

    favicon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,7 +39,7 @@ function App() {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    const timer = setTimeout(() => setLoading(false), 3000)
+    const timer = setTimeout(() => setLoading(false), 2500)
     return () => clearTimeout(timer);
   }, [setLoading])
 
